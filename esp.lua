3a735d742c7b6935887f01143cfb5a400aa79b62
morfinovaatan-oss <morfinovaatan@gmail.com>
2026-07-29 11:58:45 +0300

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,1 +1,23 @@
+local Module = {}
 
+function Module:Init(Firola)
+    local Services = Firola.Services
+    local Players = Services.Players
+    local Workspace = Services.Workspace
+    local Lighting = Services.Lighting
+    local CoreGui = Services.CoreGui
+    local LocalPlayer = Firola.LocalPlayer
+    local UI = Firola.UI
+    local tabName = "ESP"
+
+    local Config = {
+        ESP = false,
+        GunESP = false,
+        NightMode = false,
+        Fullbright = false,
+    }
+
+    -- ... (код ESP из старых visuals.lua, перенесённый сюда с созданием Highlight и BillboardGui)
+end
+
+return Module
